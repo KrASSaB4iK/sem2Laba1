e0f794a217d8b1410891429736a8c58262c19d68
KrASSaB4iK <KrASSaB4iK@users.noreply.github.com>
2023-03-30 18:04:08 +0300

    Ready for Linux

--- matrix.c
+++ matrix.c
@@ -1,23 +1,31 @@
 #include "matrix.h"
 #include "matrix_int.h"
-struct Matrix* newMatrix(
+#include <stdio.h>
+Matrix* newMatrix(
     size_t size,
     void* (*sum)(void*, void*),
     void* (*minus)(void*),
     void* (*multi)(void*, void*)
 ) {
-    RingInfo *mat = malloc(sizeof(RingInfo));
-    mat->size = size;
-    mat->sum = sum;
-    mat->minus = minus;
-    mat->multi = multi;
-    return mat;    
+	Matrix *mat = malloc(sizeof(Matrix));
+    mat->ringInfo = malloc(sizeof(RingInfo));
+    mat->ringInfo->size = size;
+    mat->ringInfo->sum = sum;
+    mat->ringInfo->minus = minus;
+    mat->ringInfo->multi = multi;
+    return mat; 
 }
 
 void *get_element(Matrix *mat) {
-    return (void*)(((matrixInt*)(mat->ringInfo))->x);
+    return (void*)(mat->x);
 }
 
-void set_element(Matrix *mat, int el) {
-    ((matrixInt*)(mat->ringInfo))->x = el;
+void set_element(Matrix *mat, void** el) {
+   	mat->x = el;
+}
+
+void delete_Matrix(Matrix *mat) {
+	free(mat->ringInfo);
+    free(mat->x);
+    free(mat);
 }
--- matrix_int.c
+++ matrix_int.c
@@ -9,13 +9,6 @@ void *sumInt(void *m1, void *m2) {
     return (void*)resm;
 }
 
-// void *get_elementInt(Matrix *mat) {
-//     return (void*)(((matrixInt*)(mat->ringInfo))->x);
-// }
-
-// void set_elementInt(Matrix *mat, int el) {
-//     ((matrixInt*)(mat->ringInfo))->x = el;
-// }
 
 void *minusInt(void *m1, void *m2) {
     int *int_m1 = (int*)m1;
