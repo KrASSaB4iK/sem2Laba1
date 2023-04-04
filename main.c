57288d1f229d6dcb18f7d1220f7d895a832ae955
KrASSaB4iK <KrASSaB4iK@users.noreply.github.com>
2023-04-04 14:01:55 +0300

    Print matrix

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include "matrix.h"
-#include "matrix_int.h"
+#include "matrixint.h"
 #include <stdlib.h>
 
 int main() {
-    Matrix *int_ring = newMatrix(sizeof(int), &sumInt, &minusInt, &multiInt);
+    Matrix *int_ring1 = newMatrix(sizeof(int), &sumInt, &minusInt, &multiInt);
+    Matrix *int_ring2 = newMatrix(sizeof(int), &sumInt, &minusInt, &multiInt);
     Matrix **array = new_array_Matrixs();
     int count= 1;
-    add_sizeof_matrix(int_ring);
-    set_element(int_ring, (void*)matrix_array_volumeInt(int_ring));
-    add_Matrix_in_array(array, int_ring, count);
-    delete_array_Matrix(array, count);
+    add_sizeof_matrix(int_ring1);
+    set_element(int_ring1, (void*)matrix_array_volumeInt(int_ring1));
+    add_Matrix_in_array(array, int_ring1, &count);
+    add_sizeof_matrix(int_ring2);
+    set_element(int_ring2, (void*)matrix_array_volumeInt(int_ring2));
+    add_Matrix_in_array(array, int_ring1, &count);
+    add_Matrix_in_array(array, sumInt(int_ring1, int_ring2), &count);
+    print_Matrix(array[2]);
+    delete_array_Matrix(array, &count);
 
 }
