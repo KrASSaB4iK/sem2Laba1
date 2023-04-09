ccd474ff98759f494fbc2c71f87c0b19284aa0f3
KrASSaB4iK <KrASSaB4iK@users.noreply.github.com>
2023-04-09 23:10:34 +0300

    Допилил меню и добавил сложение строк

--- matrixint.h
+++ matrixint.h
@@ -9,6 +9,6 @@ void *minusInt(Matrix ***arr, Matrix *m1, Matrix *m2, int *len);
 void *multiInt(Matrix ***arr, Matrix *m1, Matrix *m2, int *len);
 void *transInt(Matrix ***arr, Matrix *m, int *len);
 int *matrix_array_volumeInt(Matrix *mat);
-
+void *add_lineInt(Matrix *m1);
 
 #endif
--- menu.c
+++ menu.c
@@ -7,9 +7,9 @@ void menu(){
     printf("Какого типа данных будет матрица\n1 - (int)\n 2- (float)"); 
     int choise = 1;
     Matrix **array = new_array_Matrixs();
-    int count; 
+    int count = 0; 
     while(choise != 8){ 
-        printf("1 - create matrix\n2 - delete matrix\n3 - print matrix\n4 - sum matrix\n5 - mat multiplication\n6 - mat trasposition\n7 - adding to a line\n8 - quit\nenter the number:"); 
+        printf("1 - create matrix\n2 - sum matrix\n3 - minus matrix\n4 - multi matrix\n5 - print matrix\n6 - mat trasposition\n7 - adding to a line\n8 - quit\nenter the number:"); 
         scanf("%d", &choise); 
         switch(choise){ 
             case 1: 
@@ -17,20 +17,22 @@ void menu(){
                 array = choose_type_of_matrix(array, &count);
                 break; 
             case 2:  
+                array = sum_Matrix(array, &count);
                 break; 
             case 3: 
-                print_Matrix(choose_Matrix(array, &count)); 
+                array = minus_Matrix(array, &count);
                 break; 
             case 4: 
-                sum_Matrix(array, &count);
+                array = multi_Matrix(array, &count);
                 break; 
             case 5: 
-                multi_Matrix(array, &count);
+                print_Matrix(choose_Matrix(array, &count)); 
                 break; 
             case 6: 
-                trans_Matrix(array, &count);
+                array = trans_Matrix(array, &count);
                 break; 
             case 7: 
+                add_line_to_line(array, &count);
                 break; 
         } 
     } 
